8e128a479f5d73086ec2564b3294cd7fbb517f95
Clint Newsom <hcnewsom@gmail.com>
2012-12-14 19:46:56 -0500

    Hiding node title for real

--- template.php
+++ template.php
@@ -30,8 +30,11 @@ function nypl_kids_respond_form_alter(&$form, &$form_state, $form_id) {
   }
 }
 
-function nypl_kids_respon_preprocess_page(&$vars){
-  if ($vars['title'] == 'front-page') {
+/**
+ * Implementation of hook_preprocess_page
+ */
+function nypl_kids_respond_preprocess_page(&$vars){
+  if($vars['is_front']){
     $vars['title'] = '';
   }
 }
