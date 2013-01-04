e71f2454f64cd3f26ade2909265fd29e0ea5eef8
Clint Newsom <hcnewsom@gmail.com>
2013-01-04 11:31:23 -0500

    attempting to modify site name in theme

--- template.php
+++ template.php
@@ -32,7 +32,7 @@ function nypl_kids_respond_form_alter(&$form, &$form_state, $form_id) {
 
 function nypl_kids_respond_preprocess_html(&$vars) {
   $vars['head_title'] = str_replace("_", " ", $vars['head_title']);
-  $vars['head_title'] = str_replace("Kids!", "NYPL Kids", $vars['head_title']);
+  $vars['head_title'] = str_replace("| Kids!", "| NYPL Kids", $vars['head_title']);
 }
 
 
