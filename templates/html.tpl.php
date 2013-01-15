9cfcf9ab8adcd9bebdbe1671631a5cce4a23cfb3
Clint Newsom <hcnewsom@gmail.com>
2013-01-15 14:27:46 -0500

    edge mode for ie 8

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -5,7 +5,11 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
-  <meta http-equiv="X-UA-Compatible" content="IE=7">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!--[if lt IE 7]>  <html class="no-js ie ie6 lte9 lte8 lte7"> <![endif]-->
+  <!--[if IE 7]>     <html class="no-js ie ie7 lte9 lte8 lte7"> <![endif]-->
+  <!--[if IE 8]>     <html class="no-js ie ie8 lte9 lte8"> <![endif]-->
+  <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body<?php print $attributes;?>>
   <div id="skip-link">
