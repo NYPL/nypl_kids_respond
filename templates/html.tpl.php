2172cb67fe7da4f798b313754e2a4b948d8afafa
Clint Newsom <hcnewsom@gmail.com>
2013-01-15 13:09:36 -0500

    checks for ie 8

--- templates/html.tpl.php
+++ templates/html.tpl.php
@@ -5,6 +5,9 @@
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
   <?php print $scripts; ?>
+  <!--[if lt IE 7]>  <html class="no-js ie ie6 lte9 lte8 lte7"> <![endif]-->
+  <!--[if IE 7]>     <html class="no-js ie ie7 lte9 lte8 lte7"> <![endif]-->
+  <!--[if IE 8]>     <html class="no-js ie ie8 lte9 lte8"> <![endif]-->
   <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 </head>
 <body<?php print $attributes;?>>
